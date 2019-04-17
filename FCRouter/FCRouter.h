38a8767715af08121b99dec96ed4de49d56f2255
ForC <heqiao.china@gmail.com>
2019-04-17 18:33:38 +0800

    feat: 可有可无的注释

--- FCRouter/FCRouter.h
+++ FCRouter/FCRouter.h
@@ -36,24 +36,72 @@ FOUNDATION_EXPORT NSString *FCRouterKey;
 
 + (instancetype)share;
 
+/**
+ 通过plist注册模块或页面间映射关系
+
+ @param source 路径
+ @param bundle bundle
+ */
 - (void)regsiterPlistPathForSource:(NSString *)source bundle:(NSBundle *)bundle;
 
+/**
+ 注册一条模块映射关系
+
+ @param url         url-router
+ @param VCClass     class
+ */
 - (void)regsiterUrl:(NSString *)url mapViewControllerClass:(Class)VCClass;
 
+/**
+ 注册一条业务处理映射关系
+ 
+ @param url         url-router
+ @param handle      handle
+ */
 - (void)regsiterUrl:(NSString *)url mapHandle:(FCRouterHandle)handle;
 
+/**
+ 匹配一条映射关系
+ 
+ @param url         url-router
+ */
 - (UIViewController *)matchViewControllerWithUrl:(NSString *)url;
 
+/**
+ 匹配一条映射关系
+ 
+ @param url         url-router
+ @param userInfo    传递非常规对象
+ */
 - (UIViewController *)matchViewControllerWithUrl:(NSString *)url userInfo:(NSDictionary *)userInfo;
 
 - (UIViewController *)openUrl:(NSString *)url;
 
+/**
+ 匹配一条业务处理
+ 
+ @param url         url-router
+ */
 - (id)matchHandleWithUrl:(NSString *)url;
 
+/**
+ 匹配一条业务处理
+ 
+ @param url         url-router
+ @param userInfo    传递非常规对象
+ */
 - (id)matchHandleWithUrl:(NSString *)url userInfo:(NSDictionary *)userInfo;
 
+/**
+ 删除一条业务处理
+ 
+ @param url         url-router
+ */
 - (void)removeHandleWithUrl:(NSString *)url;
 
+/**
+ 判断是否可打开
+ */
 + (BOOL)canOpenUrl:(NSString *)url;
 
 @end
